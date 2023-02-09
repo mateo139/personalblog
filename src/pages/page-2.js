903dafede2a20a96346dd83623d3faee7cc0b4bc
Mateo <103432222+mateo139@users.noreply.github.com>
2023-02-09 14:17:39 +0100

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,10 +8,6 @@ const SecondPage = () => (
   <Layout>
     <h1>Hi Stranger! This page is about myself</h1>
     <p>I am a graduate Civil Engineer, but in fact, the highest satisfaction is given me by building bridges between people and technology.<p>
-    <p>I have experience in technical sales of Calenberg elastomeric bearings for the building engineering industry.<p>
-    <p>That is why I understand the business needs of manufacturers and clients.<p>
-    <p>It's a great passion for me to develop and implement IT solutions, to make the work of people easier, by automating them.<p>
-    <p>I am willing to act as an intermediary between the world of engineers and software developers.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
